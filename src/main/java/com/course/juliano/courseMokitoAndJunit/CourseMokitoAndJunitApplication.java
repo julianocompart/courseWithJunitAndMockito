957c83770d0c54c8fd52d438aa203a24b4522395
julianocompart <juliano.csn@gmail.com>
2022-12-06 22:29:20 -0300

    initiating project

---
+++ src/main/java/com/course/juliano/courseMokitoAndJunit/CourseMokitoAndJunitApplication.java
@@ -0,0 +1,13 @@
+package com.course.juliano.courseMokitoAndJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMokitoAndJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMokitoAndJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/juliano/courseMokitoAndJunit/CourseMokitoAndJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.juliano.courseMokitoAndJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMokitoAndJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
